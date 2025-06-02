972904aa716fbcafdc7abaf024a4a142b3f34c0c
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-06-02 18:24:06 +0000

    readme.md file update

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -11,29 +11,25 @@ resource "aws_db_subnet_group" "rds" {
 }
 
 resource "aws_db_instance" "mysql" {
-  identifier           = "demo-rds"
-  allocated_storage    = var.allocated_storage
-  engine               = var.engine
-  engine_version       = var.engine_version
-  instance_class       = var.instance_class
-  db_name              = var.db_name
-  username             = var.username
-  password             = var.password
-  publicly_accessible  = false
-  db_subnet_group_name = aws_db_subnet_group.rds.name
-  vpc_security_group_ids = var.vpc_security_group_ids
-  #skip_final_snapshot  = true
+  identifier               = "demo-rds"
+  allocated_storage        = var.allocated_storage
+  engine                   = var.engine
+  engine_version           = var.engine_version
+  instance_class           = var.instance_class
+  db_name                  = var.db_name
+  username                 = var.username
+  password                 = var.password
+  publicly_accessible      = false
+  db_subnet_group_name     = aws_db_subnet_group.rds.name
+  vpc_security_group_ids   = var.vpc_security_group_ids
 
-  # Backups are required in order to create a replica
-  maintenance_window      = "Mon:00:00-Mon:03:00"
-  backup_window           = "03:00-06:00"
-  backup_retention_period = 7
-
-  # Enable automated backups
-  skip_final_snapshot = false
-  multi_az = true
+  # Required for read replica creation
+  backup_retention_period  = 7
+  skip_final_snapshot      = false
   final_snapshot_identifier = "db-snap"
-
+  multi_az                 = true
+  maintenance_window       = "Mon:00:00-Mon:03:00"
+  backup_window            = "03:00-06:00"
 
   tags = merge(
     var.project_tags,
@@ -43,13 +39,13 @@ resource "aws_db_instance" "mysql" {
   )
 }
 
-
 resource "aws_db_instance" "demo-rds-read" {
-  identifier             = "demo-rds-read"
-  replicate_source_db    = aws_db_instance.mysql.identifier
-  instance_class         = "db.t3.micro"
-  skip_final_snapshot    = true
-# disable backups to create DB faster
+  identifier              = "demo-rds-read"
+  replicate_source_db     = aws_db_instance.mysql.identifier
+  instance_class          = "db.t3.micro"
+  skip_final_snapshot     = true
+
+  # Not required, but keeps a backup for replica in case of promotion
   backup_retention_period = 1
 
   tags = merge(
@@ -58,5 +54,6 @@ resource "aws_db_instance" "demo-rds-read" {
       Name = "replica-rds"
     }
   )
-  depends_on = [aws_db_instance.mysql]  
+
+  depends_on = [aws_db_instance.mysql]
 }
